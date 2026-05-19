ff31e71f2e918a999b1f600f242daddd079db620
Domenico Scibilia <sonodomy01@gmail.com>
2026-05-19 11:36:24 +0200

    Prova di modifica del file

--- src/main/java/com/gamehub/arena/controller/AuthController.java
+++ src/main/java/com/gamehub/arena/controller/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/gameHubArena/gameHub")
+@RequestMapping("/api/auth")
 public class AuthController {
     @GetMapping("/test")
     public String test(){
