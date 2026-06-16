97b35cb08f54a38c39dae1ead76ea77aa76b371e
Matteo <clnmtt04b02m208x@studenti.unical.it>
2026-06-16 19:21:55 +0200

    Modifiche

--- src/main/java/com/gamehub/arena/controller/AuthController.java
+++ src/main/java/com/gamehub/arena/controller/AuthController.java
@@ -54,6 +54,8 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestBody UserLoginDTO dto) {
         Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword()));
         UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        User user = userService.findByUsername(userDetails.getUsername()).orElseThrow();
+
 
         String authority = userDetails.getAuthorities().iterator().next().getAuthority();
 
@@ -66,6 +68,7 @@ public class AuthController {
         res.setUsername(userDetails.getUsername());
         res.setRole(userDetails.getAuthorities().iterator().next().getAuthority());
         res.setToken(token);
+        res.setId(user.getId());
 
         return ResponseEntity.ok(res);
     }
--- src/main/java/com/gamehub/arena/dto/GameExternalDTO.java
+++ src/main/java/com/gamehub/arena/dto/GameExternalDTO.java
@@ -12,5 +12,6 @@ public class GameExternalDTO {
     private String genere;
     private String trailerUrl;
     private String slug;
-    
+    private String description;
+
 }
